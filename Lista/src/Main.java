fe37c6608340b103235f46920fb310cbfe23fd89
cmasana <unaltrataronja@gmail.com>
2020-11-18 19:01:25 +0100

    Añadidas funciones
    Insertar(), Localizar() y varias para imprimir e insertar valores

--- Lista/src/Main.java
+++ Lista/src/Main.java
@@ -14,6 +14,11 @@ public class Main {
         Scanner consola = new Scanner(System.in);
 
         /*
+        Controla el nº de elementos que se encuentran dentro del array
+        */
+        int contadorElementos = 0;
+
+        /*
         Número máximo de apellidos en la lista
          */
         final int MAX = 5;
@@ -21,7 +26,7 @@ public class Main {
         /*
         Array con la lista de apellidos de los alumnos de clase
          */
-        String [] listaAlumnos = new String[MAX];
+        String[] listaAlumnos = new String[MAX];
 
         /*
         Entero que almacena la posición de un determinado elemento que se encuentra en el array
@@ -45,41 +50,42 @@ public class Main {
             /*
             Menú principal con opciones
              */
-            String [] menu = {
-                    "(1) Insertar un apellido en la lista",
-                    "(2) Localizar la posición de un apellido",
-                    "(3) Localizar un apellido",
-                    "(4) Eliminar un apellido",
-                    "(5) Eliminar todos los apellidos iguales",
-                    "(6) Vaciar la lista de apellidos",
-                    "(7) Mostrar primer/último apellido",
-                    "(8) Listar apellidos con su posición",
-                    "(9) Ordenar la lista de apellidos",
-                    "(0) Salir"
+            String[] menu = {
+                    "Insertar un apellido en la lista",
+                    "Localizar la posición de un apellido",
+                    "Localizar un apellido",
+                    "Eliminar un apellido",
+                    "Eliminar todos los apellidos iguales",
+                    "Vaciar la lista de apellidos",
+                    "Mostrar primer/último apellido",
+                    "Listar apellidos con su posición",
+                    "Ordenar la lista de apellidos",
+                    "Salir"
             };
 
             /*
             Imprime las opciones del menú principal
              */
-            for (int i = 0; i < menu.length; i++) {
-                System.out.println(menu[i]);
-            }
+            printMenu(menu);
 
             /*
             Opción seleccionada por el usuario
              */
-            System.out.println("Escoge una opción:");
-            opcion = consola.nextInt();
+            printText("Escoge una opción:");
+            opcion = enterInt();
 
             /*
-            Menú con las opciones a realizarse
+            Permite ejecutar diferentes instrucciones según la opción seleccionada
              */
             switch (opcion) {
                 case 1: // Insertar
-                    System.out.println("Insertar un apellido en la lista");
+                    /*
+                    Asignamos un nuevo valor a contadorElementos para poder utilizarlo durante la ejecución del programa
+                     */
+                    contadorElementos = insertar(listaAlumnos, contadorElementos);
                     break;
                 case 2: // Localizar posición
-                    System.out.println("Localizar la posición de un apellido");
+                    contadorElementos = localizar(listaAlumnos, contadorElementos);
                     break;
                 case 3: // localizar elemento
                     System.out.println("Localizar un apellido");
@@ -99,23 +105,21 @@ public class Main {
                         Menú secundario para primer y último apellido en la lista
                          */
                         String [] menuPrimeroUltimo = {
-                                "(1) Muestra primer apellido de la lista",
-                                "(2) Muestra último apellido de la lista",
-                                "(3) Atrás"
+                                "Muestra primer apellido de la lista",
+                                "Muestra último apellido de la lista",
+                                "Atrás"
                         };
 
                         /*
-                        Bucle que muestra las opciones del menú por pantalla
+                        Muestra las opciones del menú por pantalla
                          */
-                        for (int i = 0; i < menuPrimeroUltimo.length; i++) {
-                            System.out.println(menuPrimeroUltimo[i]);
-                        }
+                        printMenu(menuPrimeroUltimo);
 
                         /*
                         Muestra mensaje al usuario
                          */
-                        System.out.println("Escoge una opción: ");
-                        opcion = consola.nextInt();
+                        printText("Escoge una opción: ");
+                        opcion = enterInt();
 
                         switch (opcion) {
                             case 1: // Muestra primer elemento
@@ -129,7 +133,6 @@ public class Main {
                                 break;
                             default: // Opción incorrecta
                                 System.out.println("Opción incorrecta");
-                                opcion = consola.nextInt();
                                 break;
                         }
                     }
@@ -148,23 +151,21 @@ public class Main {
                         Menú secundario para hacer gestiones en la lista ordenada
                          */
                         String [] menuOrdenar = {
-                                "(1) Busqueda en lista ordenada de apellidos",
-                                "(2) Eliminar apellidos desde lista ordenada",
-                                "(3) Atrás"
+                                "Busqueda en lista ordenada de apellidos",
+                                "Eliminar apellidos desde lista ordenada",
+                                "Atrás"
                         };
 
                         /*
-                        Bucle que muestra las opciones del menú
+                        Muestra las opciones del menú por pantalla
                          */
-                        for (int i = 0; i < menuOrdenar.length; i++) {
-                            System.out.println(menuOrdenar[i]);
-                        }
+                        printMenu(menuOrdenar);
 
                         /*
                         Mostrar mensaje al usuario para introducir una nueva opción
                          */
-                        System.out.println("Escoge una opción: ");
-                        opcion = consola.nextInt();
+                        printText("Escoge una opción: ");
+                        opcion = enterInt();
 
                         switch (opcion) {
                             case 1: // Localizar en lista ordenada
@@ -178,23 +179,182 @@ public class Main {
                                 break;
                             default: // Opción incorrecta
                                 System.out.println("Opción incorrecta");
-                                opcion = consola.nextInt();
+                                opcion = enterInt();
                                 break;
                         }
                     }
                     while (opcion != 3);
                     break;
-                case 0: // Salir
-                    System.out.println("Salir");
+                case 10: // Salir
                     System.exit(0);
                     break;
                 default: // opción incorrecta
                     System.out.println("Opción incorrecta!!");
-                    opcion = consola.nextInt();
                     break;
             }
         }
 
-        while (opcion != 0);
+        while (opcion != 10);
+    }
+
+
+    /**
+     * Permite introducir un elemento en una determinada posición, ambos introducidos por el usuario
+     *
+     * @param lista array de Strings
+     * @param contadorElementos entero que permite contabilizar el nº de elementos introducidos en el array
+     * @return devuelve el valor actualizado de contadorElementos
+     */
+    public static int insertar(String[] lista, int contadorElementos) {
+        String elemento;
+        int posicion;
+
+        printText("Introduce un apellido");
+        elemento = enterString();
+
+        printText("Introduce una posición");
+        posicion = enterInt();
+
+        /*
+        Comprobación: Si en la posición introducida ya existe un valor almacenado
+         */
+        if (lista[posicion] != null) {
+            printText("Error: La posición introducida está siendo utilizada.");
+
+        /*
+        Comprobación: Si el array está completo(lleno)
+         */
+        } else if (contadorElementos == lista.length) {
+            printText("Error: El array está lleno.");
+
+        /*
+        Comprobación: Si la posición introducida es mayor que 0, comprobamos que el valor de la posición anterior no sea
+        null
+         */
+        } else if (posicion > 0 && lista[posicion - 1] == null) {
+            printText("Error: La posición anterior no contiene valores.");
+
+        /*
+        Si lo anterior no se cumple, podemos introducir el valor en el array
+         */
+        } else {
+            /*
+            Añadimos elemento al array en la posición indicada
+             */
+            lista[posicion] = elemento;
+
+            /*
+            Incrementamos en 1 el número de elementos del array
+             */
+            contadorElementos++;
+        }
+
+        /*
+        Mostramos array por pantalla
+         */
+        printArrayNum(lista);
+
+        /*
+        Devolvemos el valor de contadorElementos
+         */
+        return contadorElementos;
+    }
+
+    /**
+     * Localiza la primera posición en la que se encuentra un dterminado elemento introducido por el usuario
+     *
+     * @param lista array de Strings con la lista de apellidos
+     * @param contadorElementos entero que permite contabilizar el nº de elementos introducidos en el array
+     * @return devuelve el valor actualizado de contadorElementos
+     */
+    public static int localizar(String[] lista, int contadorElementos) {
+        boolean status = false;
+        String elemento;
+        int posicion;
+
+        printText("Introduce un apellido para buscar");
+        elemento = enterString();
+
+        for (int i = 0; i < contadorElementos; i++) {
+            if (lista[i].equals(elemento)){
+                posicion = i;
+                printText("La posición del apellido " + elemento + " es: " + posicion);
+                status = true;
+                break; // Rompe la ejecución del bucle cuando encuentra el primer valor
+            }
+        }
+
+        if (!status) {
+            printText("No se han encontrado coincidencias!!");
+        }
+        return contadorElementos;
+    }
+
+    /**
+     * Imprime cada elemento de un array (formato con números)
+     * @param array array de Strings
+     */
+    public static void printArrayNum(String[] array) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] != null) {
+                System.out.println(i + ". " + array[i]);
+            }
+        }
+    }
+
+    /**
+     * Imprime cada elemento de un array (formato sin números)
+     * @param array array de Strings
+     */
+    public static void printMenu(String[] array) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] != null) {
+                System.out.println((i + 1) + ". " + array[i]);
+            }
+        }
+    }
+
+    /**
+     * Muestra por pantalla un texto
+     *
+     * @param mensaje texto que queremos mostrar por pantalla
+     */
+    public static void printText(String mensaje) {
+        System.out.println(mensaje);
+    }
+
+    /**
+     * Permite al usuario introducir un número entero desde consola
+     *
+     * @return un numero entero
+     */
+    public static int enterInt() {
+        Scanner consola = new Scanner(System.in);
+        String digito;
+        int numero;
+
+        do {
+            digito = consola.next();
+        } while (digito.isEmpty());
+
+        numero = Integer.parseInt(digito);
+
+        return numero;
+    }
+
+    /**
+     * Permite al usuario introducir un String desde consola
+     *
+     * @return una cadena de caracteres
+     */
+    public static String enterString() {
+        Scanner consola = new Scanner(System.in);
+        String texto;
+
+        do {
+            texto = consola.nextLine();
+        } while (texto.isEmpty());
+
+        return texto;
     }
 }
